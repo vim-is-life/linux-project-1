0860fc111fcec3b10f04dda833001be1f69ed6e4
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2023-02-28 10:38:16 -0600

    updated instructional files

---
+++ README.md
@@ -0,0 +1,13 @@
+# linux-project-1
+Script is intended to run to set up the pi zero for my project from the moment
+after user initialization.
+
+Wifi should be set up and git installed before running this script. Download it like the below.
+``` shell
+git clone https://github.com/vim-is-life/linux-project-1.git
+```
+
+And then run the script like the below
+``` shell
+./setup.sh
+```
--- commands.sh
+++ commands.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env sh
+
+# 123149
+# 1 March 2023
+# Linux Project 1
+
+# Script is intended to run to set up the pi zero for my project from the moment
+# after user initialization.
+#
+# Wifi should be set up and git installed before running this script. Download it like the below.
+#   git clone https://github.com/vim-is-life/linux-project-1.git
+# And then run the script like the below
+#   ./setup.sh
